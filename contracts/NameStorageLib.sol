ec434d4f9d1914678145103df3a8996521b1c461
Janis Kirsteins <janis.kirsteins@notakey.com>
2017-10-10 18:16:36 +0300

    WIP refactoring name storage out of ClaimRegistry

---
+++ contracts/NameStorageLib.sol
@@ -0,0 +1,59 @@
+pragma solidity ^0.4.15;
+
+library NameStorageLib {
+
+    struct Store {
+        mapping (uint => bytes32[]) ixToName;
+        uint nameCount;
+    }
+
+    function getNameCount(Store storage self) constant returns (uint) {
+        return self.nameCount;
+    }
+
+    function getNameChunkAt(Store storage self, uint ix, uint chunkIndex) constant returns (bytes32) {
+        return self.ixToName[ix][chunkIndex];
+    }
+
+    function getNameIndex(Store storage self, bytes32[] schemaNameParts) constant returns (int) {
+        bytes memory schemaName = bytes32ArrayToBytes(schemaNameParts);
+        for (uint i = 0; i < self.nameCount; ++i) {
+            bytes memory currentSchemaName = bytes32ArrayToBytes(self.ixToName[i]);
+            if (sha3(currentSchemaName) == sha3(schemaName)) {
+                return int(i);
+            }
+        }
+        return -1;
+    }
+
+    function submitName(Store storage self, bytes32[] schemaNameParts) returns (bytes32, uint) {
+        require(getNameIndex(self, schemaNameParts) == -1);
+
+        var newIx = self.nameCount;
+        self.nameCount += 1;
+
+        self.ixToName[newIx] = schemaNameParts;
+
+        bytes memory schemaName = bytes32ArrayToBytes(schemaNameParts);
+        return (keccak256(schemaName), newIx);
+    }
+
+    function bytes32ArrayToBytes (bytes32[] data) constant returns (bytes) {
+        bytes memory bytesString = new bytes(data.length * 32);
+        uint urlLength;
+        for (uint i=0; i<data.length; i++) {
+            for (uint j=0; j<32; j++) {
+                byte char = byte(bytes32(uint(data[i]) * 2 ** (8 * j)));
+                if (char != 0) {
+                    bytesString[urlLength] = char;
+                    urlLength += 1;
+                }
+            }
+        }
+        bytes memory bytesStringTrimmed = new bytes(urlLength);
+        for (i=0; i<urlLength; i++) {
+            bytesStringTrimmed[i] = bytesString[i];
+        }
+        return bytesStringTrimmed;
+    }
+ }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,12 +3,14 @@
 // var ZoneSet = artifacts.require("./ZoneSet.sol");
 var KeyProofs = artifacts.require("./KeyProofs.sol");
 var ClaimRegistry = artifacts.require("./ClaimRegistry.sol");
+var NameStorageLib = artifacts.require("./NameStorageLib.sol");
 
 module.exports = function(deployer) {
   // deployer.deploy(TestContract, 3); 
   // deployer.deploy(AddressSet); 
   // deployer.deploy(ZoneSet); 
   deployer.deploy(KeyProofs);
+  deployer.deploy(NameStorageLib);
 
   // TODO: claimregistry doesnt query keyproofs
   deployer.deploy(ClaimRegistry/*, KeyProofs.address*/);
