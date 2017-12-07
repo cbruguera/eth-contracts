99ab6288b75e87778a023320bc9f1323e4385c1c
Janis Kirsteins <janis.kirsteins@notakey.com>
2017-12-07 14:40:28 +0200

    Removing warnings

--- contracts/TestVerifier.sol
+++ contracts/TestVerifier.sol
@@ -4,11 +4,11 @@ import './NotakeyVerifierV1.sol';
 
 contract TestVerifier is NotakeyVerifierV1 {
 
-    function TestVerifier(address trustedIssuer, address _claimRegistry) NotakeyVerifierV1(trustedIssuer, _claimRegistry) {
+    function TestVerifier(address trustedIssuer, address _claimRegistry) NotakeyVerifierV1(trustedIssuer, _claimRegistry) public {
         
     }
 
-    function vipFunction(uint nationBlacklist) onlyVerifiedSenders(msg.sender, nationBlacklist) {
+    function vipFunction(uint nationBlacklist) onlyVerifiedSenders(msg.sender, nationBlacklist) public {
         
     }
 
