4953a8c59bf3f550d2a474dc6e6238981e9f3400
Janis Kirsteins <janis.kirsteins@notakey.com>
2017-12-07 11:14:36 +0200

    Restored missing token tests

--- test/icopass_token.js
+++ test/icopass_token.js
@@ -73,60 +73,64 @@ contract('IcoPassToken', function (accounts) {
     }
   });
 
-  // it('should throw an error when trying to transferFrom more than _from has', async function () {
-  //   let balance0 = await token.balanceOf(accounts[0]);
-  //   await token.approve(accounts[1], 99);
-  //   try {
-  //     await token.transferFrom(accounts[0], accounts[2], balance0 + 1, { from: accounts[1] });
-  //     assert.fail('should have thrown before');
-  //   } catch (error) {
-  //     assertRevert(error);
-  //   }
-  // });
+  it('should throw an error when trying to transferFrom more than _from has', async function () {
+    let balance0 = await token.balanceOf(accounts[0]);
+    await token.approve(accounts[1], 99);
+    try {
+      await token.transferFrom(accounts[0], accounts[2], balance0 + 1, { from: accounts[1] });
+      assert.fail('should have thrown before');
+    } catch (error) {
+      assertRevert(error);
+    }
+  });
 
-  // describe('validating allowance updates to spender', function () {
-  //   let preApproved;
+  describe('payment distribution among holders', function () {
+    // todo
+  })
 
-  //   it('should start with zero', async function () {
-  //     preApproved = await token.allowance(accounts[0], accounts[1]);
-  //     assert.equal(preApproved, 0);
-  //   });
+  describe('validating allowance updates to spender', function () {
+    let preApproved;
 
-  //   it('should increase by 50 then decrease by 10', async function () {
-  //     await token.increaseApproval(accounts[1], 50);
-  //     let postIncrease = await token.allowance(accounts[0], accounts[1]);
-  //     preApproved.plus(50).should.be.bignumber.equal(postIncrease);
-  //     await token.decreaseApproval(accounts[1], 10);
-  //     let postDecrease = await token.allowance(accounts[0], accounts[1]);
-  //     postIncrease.minus(10).should.be.bignumber.equal(postDecrease);
-  //   });
-  // });
+    it('should start with zero', async function () {
+      preApproved = await token.allowance(accounts[0], accounts[1]);
+      assert.equal(preApproved, 0);
+    });
 
-  // it('should increase by 50 then set to 0 when decreasing by more than 50', async function () {
-  //   await token.approve(accounts[1], 50);
-  //   await token.decreaseApproval(accounts[1], 60);
-  //   let postDecrease = await token.allowance(accounts[0], accounts[1]);
-  //   postDecrease.should.be.bignumber.equal(0);
-  // });
+    it('should increase by 50 then decrease by 10', async function () {
+      await token.increaseApproval(accounts[1], 5000000);
+      let postIncrease = await token.allowance(accounts[0], accounts[1]);
+      assert.equal(preApproved.plus(5000000).toNumber(), postIncrease.toNumber());
+      await token.decreaseApproval(accounts[1], 1000000);
+      let postDecrease = await token.allowance(accounts[0], accounts[1]);
+      assert.equal(postIncrease.minus(1000000).toNumber(), postDecrease.toNumber());
+    });
+  });
 
-  // it('should throw an error when trying to transfer to 0x0', async function () {
-  //   let token = await IcoPassToken.new(accounts[0], 100);
-  //   try {
-  //     await token.transfer(0x0, 100);
-  //     assert.fail('should have thrown before');
-  //   } catch (error) {
-  //     assertRevert(error);
-  //   }
-  // });
+  it('should increase by 50 then set to 0 when decreasing by more than 50', async function () {
+    await token.approve(accounts[1], 5000000);
+    await token.decreaseApproval(accounts[1], 6000000);
+    let postDecrease = await token.allowance(accounts[0], accounts[1]);
+    assert.equal(postDecrease, 0);
+  });
 
-  // it('should throw an error when trying to transferFrom to 0x0', async function () {
-  //   let token = await IcoPassToken.new(accounts[0], 100);
-  //   await token.approve(accounts[1], 100);
-  //   try {
-  //     await token.transferFrom(accounts[0], 0x0, 100, { from: accounts[1] });
-  //     assert.fail('should have thrown before');
-  //   } catch (error) {
-  //     assertRevert(error);
-  //   }
-  // });
+  it('should throw an error when trying to transfer to 0x0', async function () {
+    let token = await IcoPassToken.new(accounts[0]);
+    try {
+      await token.transfer(0x0, 100);
+      assert.fail('should have thrown before');
+    } catch (error) {
+      assertRevert(error);
+    }
+  });
+
+  it('should throw an error when trying to transferFrom to 0x0', async function () {
+    let token = await IcoPassToken.new(accounts[0]);
+    await token.approve(accounts[1], 100);
+    try {
+      await token.transferFrom(accounts[0], 0x0, 10000000, { from: accounts[1] });
+      assert.fail('should have thrown before');
+    } catch (error) {
+      assertRevert(error);
+    }
+  });
 });
