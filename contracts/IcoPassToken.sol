90cc869cfec3a2c9cedef58e60750056fe85990c
Janis Kirsteins <janis.kirsteins@notakey.com>
2017-12-07 11:14:23 +0200

    Added payment distribution function to the token contract

--- contracts/IcoPassToken.sol
+++ contracts/IcoPassToken.sol
@@ -1,8 +1,9 @@
 pragma solidity ^0.4.13;
 
+import './DSMath.sol';
 import 'zeppelin-solidity/contracts/token/StandardToken.sol';
 
-contract IcoPassToken is StandardToken {
+contract IcoPassToken is StandardToken, DSMath {
 
   string public constant name = "IcoPassToken";
   string public constant symbol = "NIP";
@@ -10,12 +11,30 @@ contract IcoPassToken is StandardToken {
 
   uint256 public constant INITIAL_SUPPLY = 10000 * (10 ** uint256(decimals));
 
+  address[] private holders;
+
   /**
    * @dev Constructor that gives msg.sender all of existing tokens.
    */
   function IcoPassToken(address _treasury) public {
     totalSupply = INITIAL_SUPPLY;
     balances[_treasury] = INITIAL_SUPPLY;
+  }
+
+  function distributeAmongHolders() payable {
+    require(msg.value > 0);
+    uint accountedFor = 0;
+
+    for (uint i = 0; i < holders.length; ++i) {
+      address holder = holders[i];
+      uint holderBalance = balances[holder];
+      uint holderShare = wdiv(holderBalance, INITIAL_SUPPLY);
+      uint holderValue = wmul(msg.value, holderShare);
+      accountedFor += holderValue;
+      holder.transfer(holderValue);
+    }
+
+    require(accountedFor == msg.value);
   }
 
 }
