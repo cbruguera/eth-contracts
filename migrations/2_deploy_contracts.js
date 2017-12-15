ec180b5efef7c5683d1f2ed4053db9d5289eb3a5
Janis Kirsteins <janis.kirsteins@notakey.com>
2017-12-15 12:59:20 +0200

    Fixed deploy contract - it was missing the initial 5 schemas
    Schemas 1...5 are needed, otherwise the IcoContributor schema
    does not have the expected index

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -47,23 +47,23 @@ module.exports = function(deployer) {
   });
   
    deployer.then(async _ => {
-     let nsf = await NameStorageFacade.deployed();
+    let nsf = await NameStorageFacade.deployed();
 
-     [
-       "http://schema.org/Person",               // 1:0
-       "http://schema.org/Organization",         // 2:0
-       "http://kyc.notakey.com/PublicKey",       // 3:0
-       "http://kyc.notakey.com/Subject",         // 4:0
-       "http://kyc.notakey.com/Validator",       // 5:0
-       "http://kyc.notakey.com/IcoContributor"   // 6:0
-     ].forEach(async function( schemaName) {
+    for (let schemaName of [
+      "http://schema.org/Person",               // 1:0
+      "http://schema.org/Organization",         // 2:0
+      "http://kyc.notakey.com/PublicKey",       // 3:0
+      "http://kyc.notakey.com/Subject",         // 4:0
+      "http://kyc.notakey.com/Validator",       // 5:0
+      "http://kyc.notakey.com/IcoContributor"   // 6:0
+     ]) {
        let parts = schemaName.match(/.{1,32}/g);
        let encodedParts = parts.map(x => web3.fromAscii(x));
        await nsf.submitName(0, encodedParts);
        console.log("Submitted schema " + schemaName);
-     });
+     };
 
-    [
+    for (let attrName of [
       "name",             // 1:1
       "keyBlob",          // 2:1 // 
       "keyType",          // 3:1
@@ -71,24 +71,18 @@ module.exports = function(deployer) {
       "passportPicture",  // 5:1
       "reportBundleV1",   // 6:1
       "nationalityIndex" // 7:1
-    ].forEach(async function( attrName) {
+    ]) {
       let parts = attrName.match(/.{1,32}/g);
       let encodedParts = parts.map(x => web3.fromAscii(x));
       await nsf.submitName(1, encodedParts);
       console.log("Submitted attr " + attrName);
-    });
+    };
+    
+    console.log("Deploying demo KYC claims for the test IDP key");
+    let claimRegistry = await ClaimRegistry.deployed();
+    await claimRegistry.submitClaim("0xd46c9b407afa991c4c510ab5ebf8959ea7409c17", 
+    6, 6 /*report bundle*/, 1);
+    await claimRegistry.submitClaim("0xd46c9b407afa991c4c510ab5ebf8959ea7409c17", 
+    6, 7 /*nationality*/, 1);
   });
-
-  // // Demo KYC attributes for idp.key
-  // // THIS WILL NOT WORK FOR IDP -> TESTKEY
-  // deployer.then(async _ => {
-    
-  //   let claimRegistry = await ClaimRegistry.deployed();
-
-  //   await claimRegistry.submitClaim("0xd46c9b407afa991c4c510ab5ebf8959ea7409c17", 
-  //   6, 6 /*report bundle*/, 1);
-  //   await claimRegistry.submitClaim("0xd46c9b407afa991c4c510ab5ebf8959ea7409c17", 
-  //   6, 7 /*nationality*/, 1);
-    
-  // });
 };
